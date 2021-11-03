e9fa192828c302d01f82ae8a5e4aa56ac59825ad
Sergio Pacheco <sergiomiguelpacheco64@gmail.com>
2021-11-03 16:36:03 -0600

    Update site-js.js

--- js/site-js.js
+++ js/site-js.js
@@ -10,17 +10,6 @@ $(document).ready(function ()
     $("#globalFooter").load("./global-footer.html");
 });
 
-$(".hamburger").click(function () 
-{
-    $(".hamburger").toggleClass("active");
-    screenOverlay.classList.toggle("active");
-    pageBody.classList.toggle("no-scroll");
-});
-$("#mobileNav .nav-link").click(function () 
-{
-    $(".hamburger").click();
-});
-
 // Initialization for the AOS CSS Animations
 AOS.init
 ({
