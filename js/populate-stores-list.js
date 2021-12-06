1a2e452979b52dc78a901c5b4d07455293806971
Sergio Pacheco <sergiomiguelpacheco64@gmail.com>
2021-12-06 14:20:31 -0700

    Add files via upload

--- js/populate-stores-list.js
+++ js/populate-stores-list.js
@@ -9,48 +9,6 @@ var ukStores;
 var usContainer = document.getElementById("usStoreList");
 var ukContainer = document.getElementById("ukStoreList");
 
-// ajax Calls to JSON Files
-$(document).ready(function () 
-{
-    // Call to US Store List JSON
-    $.ajax
-    ({
-        url: "./json/us-stores.json",
-        type: "GET",
-        dataType: 'json',
-        success: function(data)
-        {
-            result = data;
-            usStores = result;
-            console.log(result);
-        },
-        error: function(error)
-        {
-            console.log(error);
-        }
-    });
-
-    // Call to US Store List JSON
-    $.ajax
-    ({
-        url: "./json/uk-stores.json",
-        type: "GET",
-        dataType: 'json',
-        success: function(data)
-        {
-            result = data;
-            ukStores = result;
-            console.log(result);
-        },
-        error: function(error)
-        {
-            console.log(error);
-        }
-    });
-
-    createLists();
-});
-
 function createLists() 
 {
     var usStoreSet = "";
@@ -112,3 +70,45 @@ function createLists()
     });
     ukContainer.innerHTML = ukStoreSet;
 }
+
+// ajax Calls to JSON Files
+$(document).ready(function () 
+{
+    // Call to US Store List JSON
+    $.ajax
+    ({
+        url: "./json/us-stores.json",
+        type: "GET",
+        dataType: 'json',
+        success: function(data)
+        {
+            result = data;
+            usStores = result;
+            console.log(result);
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+    })
+
+    // Call to US Store List JSON
+    $.ajax
+    ({
+        url: "./json/uk-stores.json",
+        type: "GET",
+        dataType: 'json',
+        success: function(data)
+        {
+            result = data;
+            ukStores = result;
+            console.log(result);
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+    })
+
+    createLists();
+});
